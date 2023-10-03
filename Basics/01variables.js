28b6b007836db90d341ac19ee1f9e31937c2a694
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-03 06:32:01 +0000

    01Variables

---
+++ Basics/01variables.js
@@ -0,0 +1,14 @@
+//variables
+const accountId = 123456
+let accountEmail = 'xyz@gmail.com'
+var accountPassword = '12345'
+var accountCity = 'Jaipur'
+
+accountCity = 'Mysore'
+accountEmail = 'hc@hc.com'
+accountPassword = '123'
+let accountState;
+//accountId = 123
+console.log(accountId)
+
+console.table([accountCity,accountEmail,accountPassword,accountState])
