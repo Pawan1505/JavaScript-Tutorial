d9c65f484055bbf91f662413a5bcb9ca1fa18f0f
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-04 06:35:06 +0000

    Datatypes

---
+++ Basics/02datatypes.js
@@ -0,0 +1,11 @@
+// DataTypes and ECMA Standards
+"use strict";//New JS
+//alert('Hello')//we are using node js and not browser
+//Code Readability
+//Primitive datatypes
+//number,bigint,string "",boolean(true,false),null(standlone value),undefined(value not assigned)
+//symbol (uniqueness)
+
+//objects
+console.log(typeof undefined);//undefined
+console.log(typeof null);//obejct output
