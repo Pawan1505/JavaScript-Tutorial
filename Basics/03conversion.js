85dc18523e133a13dc20da95568f338e1c6faf45
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-05 08:19:47 +0000

    03conversion of datatypes

---
+++ Basics/03conversion.js
@@ -0,0 +1,21 @@
+//conversion
+let score = null;
+console.log(typeof score)
+
+let valueIn = Number(score)
+console.log(typeof (valueIn))
+console.log(valueIn)
+
+//Number gets convertable
+//NaN - typeof NaN is number()
+//null gets 0
+//true 1 and false 0
+//undefined NaN
+ let isLoggedIn = "ho"
+ let booisLoggedIn = Boolean(isLoggedIn)
+ console.log(booisLoggedIn)
+
+ // 1 = true
+ // 0 = false
+// "" = false
+// "hi" = false
