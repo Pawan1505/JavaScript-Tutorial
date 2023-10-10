dc13043ca1fce900906761e2486c6e441d7510dd
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-10 07:15:48 +0000

    04StringConversion

---
+++ Basics/04stringconv.js
@@ -0,0 +1,11 @@
+// let value = 3
+// let negValue = -value
+// //console.log(typeof negValue);
+// let str1 = "hello"
+// let str2 = "John"
+// let str3 = str1+str2
+// console.log(str3);
+let x = 3
+let y = x++
+console.log(x);
+console.log(y);
