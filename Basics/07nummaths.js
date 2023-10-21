30d78c894170e272fd1b22dbb693010cb3fade63
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-21 08:31:29 +0000

    07NumandMath

---
+++ Basics/07nummaths.js
@@ -0,0 +1,11 @@
+// //Num and Maths
+// const balance=new Number(10)
+// console.log(balance.toFixed(2));
+// const anOtherNum= 100000
+// console.log(anOtherNum.toLocaleString('en-IN'));
+
+
+// Maths Library
+
+// console.log(Math);
+console.log(Math.floor(Math.random()*(20-10+1))+10);
