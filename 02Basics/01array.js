68c0c4297fadbb2e0fc511391756666783f2b838
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-10-23 05:30:33 +0000

    01array

---
+++ 02Basics/01array.js
@@ -0,0 +1,23 @@
+// array
+let myArr=[1,2,3,4,5]
+// let cricPlayer=['Dhoni','Virat','Rohit']
+// console.log(cricPlayer.length);
+// myArr.unshift(8)
+// myArr.shift()
+// console.log(myArr.indexOf(5));
+// let newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// console.log("A", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr
+);
+
+let myn2 = myArr.splice(0,2)
+console.log(myn2);
+console.log("C",myArr)
+
