3595a87a360423a11be209ed81ecd004d97fd8ae
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-11-04 08:15:08 +0000

    scope baiscs

--- 03Basics/scope.js
+++ 03Basics/scope.js
@@ -1,16 +1,27 @@
 // scopes
-let c = 50
+// let c = 50
 
-if(true){
-let a= 10
-const b = 20
-let c = 30
-console.log(c);
-}
+// if(true){
+// let a= 10
+// const b = 20
+// let c = 30
+// console.log(c);
+// }
 // scope
 
 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one(){
+    const username = "John"
+    function two(){
+        const website ="youtube"
+        console.log(website);
+    }
+    console.log(username);
+    two()
+}
+one()
