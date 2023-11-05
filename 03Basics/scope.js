dccf2a11985fd85c626c6f8774edfe0b0882a33b
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-11-05 12:50:38 +0000

    Hoisting

--- 03Basics/scope.js
+++ 03Basics/scope.js
@@ -15,13 +15,36 @@
 // console.log(b);
 // console.log(c);
 
-function one(){
+// function one(){
+//     const username = "John"
+//     function two(){
+//         const website ="youtube"
+//         console.log(website);
+//     }
+//     console.log(username);
+//     two()
+// }
+// one()
+
+// Nested Scopes
+if(true){
     const username = "John"
-    function two(){
-        const website ="youtube"
-        console.log(website);
-    }
-    console.log(username);
-    two()
+    if (username === "John"){
+        const channel = "Youtube"
+        console.log(username+channel);
+    }   
 }
-one()
+
+// ++++++++++Interesting ++++++
+
+console.log(one(1));
+function one(num){
+    return num +1;
+
+}
+
+
+const two1 = function two(num1){
+    return num1 + 1; 
+}
+console.log(two1(3));
