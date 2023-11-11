d906711cdf9f38a3e46bac86893ff35892e9ef21
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-11-11 10:29:00 +0000

    control flow

---
+++ controlflow/1.js
@@ -0,0 +1,41 @@
+// Control flow and logic flow
+// if condition
+// const isUserLoggeIN = true
+// if(2 === "2"){
+//     console.log("executed");
+// }
+// else{
+//     console.log("condition not satisfied");
+// }
+
+// <,>,<=,>=,==,!=. ===, !==
+
+// const score =200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User Power:${power}`);
+// }
+// console.log(`User Power:${power}`);
+
+// short hand notation
+const balance = 1000
+
+// // if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than");
+// }
+// else if (balance <750){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard){
+    console.log("Allow to buy");
+}
---
+++ controlflow/switchcase.js
@@ -0,0 +1,27 @@
+// switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+
+    default:
+        console.log("No month exsist for the provided number");
+        break;
+}
