f159b66f3c802f6b6013159408f0ca712b580c2b
Pawan1505 <82998175+Pawan1505@users.noreply.github.com>
2023-11-23 07:16:23 +0000

    07 filter map reduce

--- Iterators/07_loops.js
+++ Iterators/07_loops.js
@@ -4,4 +4,14 @@ const myNums=[1,2,3,4,5,6,7]
 
 let newNum = myNums.map( (num) => num+10).map( (num) => num + 1 )
 
-console.log(newNum);
+// console.log(newNum);
+
+const muNum1 = [1,2,3]
+
+const myTotal = muNum1.reduce(function (acc , currval) {
+    console.log(`acc: ${acc}and currval: ${currval}`);
+    return acc + currval;
+},0)
+
+console.log(myTotal);
+
